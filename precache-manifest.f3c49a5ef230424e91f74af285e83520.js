1d5f02149f07d6907d179c6ec647e4634b3c4c50
gdrossetto <gabriel.dadamosrossetto@gmail.com>
2020-05-02 15:33:37 -0300

    Updates

---
+++ precache-manifest.f3c49a5ef230424e91f74af285e83520.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d16e06669c4686807b698dba23cb0a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2e9cb724edc244627ef5",
+    "url": "/monsters-rolodex/static/css/main.6e54258a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9cb724edc244627ef5",
+    "url": "/monsters-rolodex/static/js/main.548c085b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
